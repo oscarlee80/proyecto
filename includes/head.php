8ed0c0142e1115b7b1d115a5546bb285479fa4fa
oscarlee80 <oscarlee80@gmail.com>
2019-04-22 07:36:02 -0300

    01

---
+++ includes/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>techHub</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Arimo:400,700|Great+Vibes" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Pinyon+Script" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
---
+++ includes/nav.php
@@ -0,0 +1,12 @@
+
+
+<!-- <ul>
+  <li><a href="default.asp">Home</a></li>
+  <li><a href="news.asp">News</a></li>
+  <li><a href="contact.asp">Contact</a></li>
+  <li><a href="about.asp">About</a></li>
+</ul> -->
+<form action="">
+  <input type="search">
+  <i class="fa fa-search"></i>
+</form>
